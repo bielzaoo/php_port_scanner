27d6b775f85ab46556cb93f45fc6e6690b5a625d
bielzaaoo <bielzao0x7@gmail.com>
2023-10-17 13:48:09 -0300

    att

--- scan.php
+++ scan.php
@@ -3,21 +3,40 @@
 // implementar opção para salvar a saída.
 // Scan em range de portas e por ai vai
 
+$ip = $argv[1];
+$ports = $argv[2];
 $open = array();
 $closed = array();
 
-function scan($ip, $port)
+function scan($ip, $port, &$open, &$closed)
 {
     if ($fp = @fsockopen($ip, $port, $err, $err_string, 1)) {
-        echo "$port OPEN\n";
-
+        // echo "$port -> ABERTA\n";
         array_push($open, $port);
-
         fclose($fp);
     } else {
+        // echo "$port -> FECHADA\n";
         array_push($closed, $port);
-        echo "$port CLOSED\n";
     } 
+}
+
+function printResult(&$open, &$closed){
+
+    if (!empty($open)) {
+        echo "[OPEN PORTS]\n";
+        foreach ($open as $port) {
+            echo "$port -> OPEN\n";
+        }
+    }
+
+    echo "\n";
+
+    if (!empty($closed)) {
+        echo "[CLOSED PORTS]\n";
+        foreach ($closed as $port) {
+            echo "$port -> CLOSED\n";
+        }
+    }
 }
 
 
@@ -26,38 +45,45 @@ if ($argc < 2) {
     die();
 }
 
-$ip = $argv[1];
+function portRange($ip, $ports, &$open, &$closed)
+{
+    if (str_contains($ports, '-')) {
+        $range = explode('-', $ports);
+        $start = (int) $range[0];
+        $stop = (int) $range[1];
 
-if (str_contains($argv[2], '-')) {
-    $range = explode('-', $argv[2]);
-    $start = (int) $range[0];
-    $stop = (int) $range[1];
+        // verifica se o stop é zero.
+        if (!$stop) {
+            echo "[!!!] Somente o inicio pode ser zero.";
+            die();
+        }
 
-    // verifica se o stop é zero.
-    if (!$stop) {
-        echo "[!!!] Somente o inicio pode ser zero.";
-        die();
-    }
-
-    // veriifica se o final é maior que o começo.
-    if ($start > $stop) {
-        $temp = $stop;
-        $stop = $start;
-        $start = $temp;
-    }
+        // veriifica se o final é maior que o começo.
+        if ($start > $stop) {
+            $temp = $stop;
+            $stop = $start;
+            $start = $temp;
+        }
 
 
-    for ($i=$start; $i <= $stop; $i++) { 
-        scan($ip, $i);
-    }
+        for ($port=$start; $port <= $stop; $port++) { 
+            scan($ip, $port, $open, $closed);
+        }
 
+       return true; 
+    } 
+    return false; 
+}
+
+if (portRange($ip, $ports, $open, $closed)) {
+    printResult($open, $closed);
     die();
+} else {
+    $all_ports = explode(',', $ports);
+    foreach ($all_ports as $port) { 
+        scan($ip, $port, $open, $closed);
+    }
+    printResult($open, $closed);
 }
 
-$ports = explode(',', $argv[2]);
-foreach ($ports as $port) { 
-    scan($ip, $port);
-}
 
-
-j
